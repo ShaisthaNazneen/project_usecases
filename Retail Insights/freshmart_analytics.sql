0c5efb4500c940eaba169959de7a126ab1d7c91c
ShaisthaNazneen <144989532+ShaisthaNazneen@users.noreply.github.com>
2026-04-14 20:08:42 +0530

    Add files via upload

---
+++ Retail Insights/freshmart_analytics.sql
@@ -0,0 +1,65 @@
+create database analytics_freshmart;
+use analytics_freshmart;
+create table categories(Category_Id int primary key,Category_Name varchar(80) not null);
+-- Products Table
+create table Products(Product_Id int primary key,Product_Name varchar(100) not null, Category_Id int,expiry_date_on date,count_of_stock int,price decimal(10,2),supplier_name varchar(100),discount_percent int,foreign key(Category_Id) references categories(Category_Id));
+-- Sales Table
+create table SalesTransactions(transaction_id int primary key,Product_Id int,quantity int,sale_date date,foreign key (Product_Id) references Products(Product_Id));
+insert into categories values
+(1001,'Frozen Foods'),
+(1002,'Household_Items'),
+(1003,'Dairy'),
+(1004,'Snacks'),
+(1005,'Beauty_Products'),
+(1006,'Stationery');
+-- Insert Products
+insert into Products values
+(1,'Fruits',1001,'2026-04-08',120,80,'chills',10),
+(2,'Ice Cream',1001,'2026-04-13',60,150,'arun',5),
+(3,'Floor cleaner',1002,'2026-10-20',70,180,'Rinn',8),
+(4,'Detergent Powder',1002,'2027-05-01',200,280,'Surf excel',10),
+(5,'Milk',1003,'2026-04-05',100,30,'Amul',3),
+(6,'Cheese',1003,'2026-04-20',100,120,'Amul',5),
+(7,'Chocolates',1004,'2026-05-20',180,40,'Cadbury',10),
+(8,'Chips',1004,'2026-06-01',180,20,'Lays',5),
+(9,'Shampoo',1005,'2026-09-01',110,100,'Loreal',10),
+(10,'Perfume',1005,'2026-10-01',50,190,'Bellavita',10),
+(11,'Pen',1006,'2027-12-01',160,10,'classmate',1),
+(12,'Books',1006,'2027-12-01',190,50,'classmate',3);
+-- Insert Sales
+insert into SalesTransactions values
+(2001,1,10,'2026-04-08'),
+(2002,3,20,'2026-03-30'),
+(2003,5,15,'2026-04-04');
+-- Fetch products that will expire within the next 7 days 
+-- and still have high stock levels
+select Product_Name,expiry_date_on,count_of_stock
+from Products
+where expiry_date_on between '2026-04-09' and '2026-04-16'
+and count_of_stock>50;
+-- 2.DEAD STOCK(No sales in last 60 days)
+select p.Product_Name
+from Products p
+left join SalesTransactions s
+on p.Product_Id = s.Product_Id
+and s.sale_date >= '2026-02-08'
+where s.transaction_id is null;
+-- 3. LAST MONTH REVENUE (March 2026)
+select c.Category_Name,
+sum(p.price*s.quantity) as total_revenue
+from SalesTransactions s
+join Products p on s.Product_Id=p.Product_Id
+join Categories c on p.Category_Id=c.Category_Id
+where s.sale_date>='2026-03-01'
+and s.sale_date<'2026-04-01'
+group by c.Category_Name
+order by total_revenue desc;
+-- 3. LAST MONTH REVENUE 
+SELECT c.Category_Name,sum(p.price * s.quantity) AS total_revenue
+FROM SalesTransactions s
+JOIN Products p ON s.Product_Id = p.Product_Id
+JOIN Categories c ON p.Category_Id = c.Category_Id
+WHERE MONTH(s.sale_date) = MONTH(CURDATE() - INTERVAL 1 MONTH)
+  AND YEAR(s.sale_date) = YEAR(CURDATE() - INTERVAL 1 MONTH)
+GROUP BY c.Category_Name
+ORDER BY total_revenue DESC;
---
+++ SocialMediaContent/Posts.py
@@ -0,0 +1,77 @@
+print("Social Media Content Sanitizer")
+def Post_TextCleaned(content,Banned_Words):
+    for word in Banned_Words:
+        content=content.replace(word,"***")
+        content=content.replace(word.capitalize(),"***")
+        content=content.replace(word.upper(),"***")
+    return content
+def extract_links(content):
+    words=content.split()
+    temp_links=[]
+    for j in words:
+        if j.startswith("http"):
+            temp_links.append(j)
+    return temp_links
+#List of Posts
+posts = [
+    ("Shaistha", "small steps lead to big results"),
+    ("Mahira", "I hate when app crashes http://helpdesk.in"),
+    ("Rahul", "Such a toxic argument happening here"),
+    ("Shaistha", "Check this http://brainly.in for concepts"),
+    ("Mahira", "Bad network had ruined my test"),
+    ("Ayra", "The  session was very helpful today!"),
+    ("Rahul", "Stop spreading hate in comments"),
+    ("Shaistha", "This platform is becoming toxic day by day"),
+    ("bety","bad bad toxic hate")
+]
+#Banned Words
+Banned_Words = ["toxic","hate", "bad", "stupid", "ugly"]
+Total_p=0
+Cleaned_p=0
+Blocked_p=0
+links = []
+users = {}
+for creator,content in posts:
+    Total_p+=1
+    if creator not in users:
+        users[creator] = 0
+
+    # No of issues
+    lower_text=content.lower()
+    issue_count=0
+    for word in Banned_Words:
+        issue_count += lower_text.count(word)
+    users[creator]+=issue_count
+    # cleaning
+    # Logic:
+    # 0 issues → SAFE
+    # 1–3 issues → CLEANED
+    # >3 issues → BLOCKED
+    clean_p = Post_TextCleaned(content, Banned_Words)
+    if issue_count==0:
+        status="SAFE"
+        final_p=clean_p
+    elif issue_count<=3:
+        status = "CLEANED"
+        Cleaned_p += 1
+        final_p=clean_p
+    else:
+        status="BLOCKED"
+        Blocked_p+=1
+        final_p="POST REMOVED"
+    # extract links
+    Links_Extracted=extract_links(content)
+    links.extend(Links_Extracted)
+    print("\nUser:", creator)
+    print("Post:", final_p)
+    print("Status:",status)
+#saving links to file
+with open("links_found.txt", "w") as file:
+    for k in links:
+        file.write(k + "\n")
+#user issue report
+print("\nUser Issue Count:")
+for u in users:
+    print(u,":",users[u])
+print("\n----------------Final Report -------------------")
+print(f"Total Posts Screened: {Total_p} | Cleaned: {Cleaned_p} | Blocked: {Blocked_p}")
